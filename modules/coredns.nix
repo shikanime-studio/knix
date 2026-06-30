e90afc3102cf748eab64e103abd0e93c5c4f9d67
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-30 19:17:56 +0200

    Fix CoreDNS never configured
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I1aab1d41876d9158bbf155bb19a7a28e6a6a6964
    ghstack-source-id: 8e91ff2b5db696a82849832e0a1601b4a65ce5ae
    Pull-Request: https://github.com/shikanime-studio/knix/pull/44

--- modules/coredns.nix
+++ modules/coredns.nix
@@ -24,20 +24,18 @@ in
     services.knix = {
       extraConfig.disable = mkIf (!cfg.coredns.enable) [ "rke2-coredns" ];
 
-      manifests = mkIf (cfg.coredns.extraConfig != { }) {
-        rke2-coredns-config.content = {
-          apiVersion = "helm.cattle.io/v1";
-          kind = "HelmChartConfig";
-          metadata = {
-            name = "rke2-coredns";
-            namespace = "kube-system";
-          };
-          spec.valuesContent = builtins.toJSON (
-            recursiveUpdate {
-              nodelocal.enabled = true;
-            } cfg.coredns.extraConfig
-          );
+      manifests.rke2-coredns-config.content = {
+        apiVersion = "helm.cattle.io/v1";
+        kind = "HelmChartConfig";
+        metadata = {
+          name = "rke2-coredns";
+          namespace = "kube-system";
         };
+        spec.valuesContent = builtins.toJSON (
+          recursiveUpdate {
+            nodelocal.enabled = true;
+          } cfg.coredns.extraConfig
+        );
       };
     };
   };
