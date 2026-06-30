8c0835e516e4df3a86de401384b8c1405003c716
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-30 19:07:22 +0200

    Fix CoreDNS values serialization
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I053b9ad045b94c1fc8f225cd2776c9ac6a6a6964
    ghstack-source-id: 95a21ae8f27e3690eab790acf943542f6c646e60
    Pull-Request: https://github.com/shikanime-studio/knix/pull/43

--- modules/coredns.nix
+++ modules/coredns.nix
@@ -6,14 +6,24 @@ let
   cfg = config.services.knix;
 in
 {
-  options.services.knix.coredns.extraConfig = mkOption {
-    type = types.attrsOf types.raw;
-    default = { };
-    description = "Extra config merged into the coredns HelmChartConfig valuesContent.";
+  options.services.knix.coredns = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable CoreDNS node caching.";
+    };
+
+    extraConfig = mkOption {
+      type = types.attrsOf types.raw;
+      default = { };
+      description = "Extra config merged into the coredns HelmChartConfig valuesContent.";
+    };
   };
 
   config = mkIf cfg.coredns.enable {
     services.knix = {
+      extraConfig.disable = mkIf (!cfg.coredns.enable) [ "rke2-coredns" ];
+
       manifests = mkIf (cfg.coredns.extraConfig != { }) {
         rke2-coredns-config.content = {
           apiVersion = "helm.cattle.io/v1";
@@ -22,9 +32,11 @@ in
             name = "rke2-coredns";
             namespace = "kube-system";
           };
-          spec.valuesContent = recursiveUpdate {
-            nodelocal.enabled = true;
-          } cfg.coredns.extraConfig;
+          spec.valuesContent = builtins.toJSON (
+            recursiveUpdate {
+              nodelocal.enabled = true;
+            } cfg.coredns.extraConfig
+          );
         };
       };
     };
