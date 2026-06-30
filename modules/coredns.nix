ba547ebc7b516e6bfebbb1a8fdaccae0b7130089
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-30 19:07:21 +0200

    Update
    [ghstack-poisoned]

--- modules/coredns.nix
+++ modules/coredns.nix
@@ -6,14 +6,24 @@ let
   cfg = config.services.knix;
 in
 {
-  options.services.knix.coredns.extraConfig = mkOption {
-    type = types.attrsOf types.raw;
-    default = { };
-    description = "Extra config merged into the coredns HelmChartConfig valuesContent.";
+  options.services.knix.coredns = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable CoreDNS node caching.";
+    };
+
+    extraConfig = mkOption {
+      type = types.attrsOf types.raw;
+      default = { };
+      description = "Extra config merged into the coredns HelmChartConfig valuesContent.";
+    };
   };
 
   config = mkIf cfg.coredns.enable {
     services.knix = {
+      extraConfig.disable = mkIf (!cfg.coredns.enable) [ "rke2-coredns" ];
+
       manifests = mkIf (cfg.coredns.extraConfig != { }) {
         rke2-coredns-config.content = {
           apiVersion = "helm.cattle.io/v1";
