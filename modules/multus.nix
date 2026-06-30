f7556163fa7667baad7e1b039b09b66ed434b867
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-30 15:43:18 +0200

    Update
    [ghstack-poisoned]

--- modules/multus.nix
+++ modules/multus.nix
@@ -23,13 +23,11 @@ in
   config = mkIf cfg.multus.enable {
     services.knix = {
       # Multus must be the first CNI plugin so it can delegate to canal
-      extraConfig = mkMerge [
-        (mkIf (cfg.role == "server") {
-          cni = mkBefore [
-            "multus"
-          ];
-        })
-      ];
+      extraConfig = mkIf (cfg.role == "server") {
+        cni = mkBefore [
+          "multus"
+        ];
+      };
 
       manifests = mkIf (cfg.multus.extraConfig != { }) {
         rke2-multus-config.content = {
