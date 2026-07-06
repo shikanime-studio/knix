f4c9b7c1b5f99e0f886a5259d0872ea961edff25
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-06 22:15:52 +0200

    Fix ETCD flag error
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I0aec648fd0fd195aba0d3ae8d30574606a6a6964

--- modules/prometheus.nix
+++ modules/prometheus.nix
@@ -22,10 +22,10 @@ in
 
   config = mkIf cfg.addons.prometheus.enable {
     services.knix.extraConfig = mkIf (cfg.role == "server") {
+      # Expose ETCD for prometheus to scrape metrics
+      etcd-arg = "listen-metrics-urls=http://0.0.0.0:2379";
       # Expose scheduler for prometheus to scrape metrics
       kube-scheduler-arg = "bind-address=0.0.0.0";
-      # Expose ETCD for prometheus to scrape metrics
-      kube-etcd-arg = "listen-metrics-urls=http://0.0.0.0:2379";
     };
   };
 }
