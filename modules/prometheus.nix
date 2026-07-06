cbcfa6020c067a46c98bbb63106534e4b5047311
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-07 01:54:40 +0200

    Fix typo on kube-controller-manager
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: If879102c9d80a7f0c88e12e7e10f99a86a6a6964

--- modules/prometheus.nix
+++ modules/prometheus.nix
@@ -24,10 +24,10 @@ in
     services.knix.extraConfig = mkIf (cfg.role == "server") {
       # Expose ETCD for prometheus to scrape metrics
       etcd-arg = "listen-metrics-urls=https://0.0.0.0:2379";
+      # Expose controller manager for prometheus to scrape metrics
+      kube-controller-manager-arg = "bind-address=0.0.0.0";
       # Expose scheduler for prometheus to scrape metrics
       kube-scheduler-arg = "bind-address=0.0.0.0";
-      # Expose controller for prometheus to scrape metrics
-      kube-controller-arg = "bind-address=0.0.0.0";
     };
   };
 }
