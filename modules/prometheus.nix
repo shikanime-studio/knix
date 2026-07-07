8bbfb3b1d9d0ce55fd584012568f7936004b58b6
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-07 17:25:29 +0200

    Fix endpoint not authorizing metrics from anonymous
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I8ebe6ec1a84e6a4e4cd057a9783bd3396a6a6964

--- modules/prometheus.nix
+++ modules/prometheus.nix
@@ -21,15 +21,65 @@ in
   };
 
   config = mkIf cfg.addons.prometheus.enable {
-    services.knix.extraConfig = mkIf (cfg.role == "server") {
-      # Expose ETCD for prometheus to scrape metrics
-      etcd-arg = [ "listen-metrics-urls=http://0.0.0.0:2381" ];
-      # Expose controller manager for prometheus to scrape metrics
-      kube-controller-manager-arg = [ "bind-address=0.0.0.0" ];
-      # Expose kube-proxy for prometheus to scrape metrics
-      kube-proxy-arg = [ "metrics-bind-address=0.0.0.0:10249" ];
-      # Expose scheduler for prometheus to scrape metrics
-      kube-scheduler-arg = [ "bind-address=0.0.0.0" ];
-    };
+    services =
+      let
+        scrapeConfigs = [
+          {
+            job_name = "kube-etcd";
+            scheme = "http";
+            metrics_path = "/metrics";
+            static_configs = [ { targets = [ "127.0.0.1:2381" ]; } ];
+          }
+          {
+            job_name = "kube-proxy";
+            scheme = "https";
+            metrics_path = "/metrics";
+            tls_config.ca_file = "/var/lib/rancher/rke2/server/tls/kube-proxy/kube-proxy.crt";
+            static_configs = [ { targets = [ "127.0.0.1:10249" ]; } ];
+          }
+          {
+            job_name = "kube-scheduler";
+            scheme = "https";
+            metrics_path = "/metrics";
+            tls_config.ca_file = "/var/lib/rancher/rke2/server/tls/kube-scheduler/kube-scheduler.crt";
+            static_configs = [ { targets = [ "127.0.0.1:10259" ]; } ];
+          }
+          {
+            job_name = "kube-controller";
+            scheme = "https";
+            metrics_path = "/metrics";
+            tls_config.ca_file = "/var/lib/rancher/rke2/server/tls/kube-controller-manager/kube-controller-manager.crt";
+            static_configs = [ { targets = [ "127.0.0.1:10257" ]; } ];
+          }
+          {
+            job_name = "rke2-supervisor";
+            scheme = "https";
+            metrics_path = "/metrics";
+            tls_config.ca_file = "/var/lib/rancher/rke2/server/tls/server-ca.crt";
+            static_configs = [ { targets = [ "127.0.0.1:9345" ]; } ];
+          }
+        ];
+      in
+      {
+        knix.extraConfig = mkIf (cfg.role == "server") {
+          # Expose controller manager for prometheus to scrape metrics
+          kube-controller-manager-arg = [
+            "authorization-always-allow-paths=/healthz,/readyz,/livez,/metrics"
+          ];
+          # Expose scheduler for prometheus to scrape metrics
+          kube-scheduler-arg = [
+            "authorization-always-allow-paths=/healthz,/readyz,/livez,/metrics"
+          ];
+          supervisor-metrics = true;
+        };
+
+        prometheus = mkIf (cfg.role == "server") {
+          inherit scrapeConfigs;
+        };
+
+        vmagent.prometheusConfig = mkIf (cfg.role == "server") {
+          scrape_configs = scrapeConfigs;
+        };
+      };
   };
 }
